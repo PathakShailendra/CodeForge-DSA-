358e374c7f6514c0a719d41320b047a82a3c1d46
Shailendra Pathak <pathakharsh813@gmail.com>
2025-02-11 18:29:32 +0530

    countdigits gfg

---
+++ src/math/countdigits.java
@@ -0,0 +1,17 @@
+package math;
+
+class Solution {
+    static int evenlyDivides(int n) {
+        // code here
+        int count = 0;
+        int org = n;
+        while (n > 0) {
+            int rem = n % 10;
+            if(rem != 0 && org % rem == 0) {
+                count++;
+            }
+            n /= 10;
+        }
+        return count;
+    }
+}
