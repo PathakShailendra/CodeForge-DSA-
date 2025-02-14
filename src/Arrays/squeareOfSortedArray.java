7ef733d8df901b4102aca86cc371144bdbac474a
Shailendra Pathak <pathakharsh813@gmail.com>
2025-02-14 18:48:15 +0530

    977 leetcode

---
+++ src/Arrays/squeareOfSortedArray.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class squeareOfSortedArray {
+    public int[] sortedSquares(int[] nums) {
+        int []res = new int[nums.length];
+        int i = 0;
+        int j = nums.length-1;
+        int p = nums.length-1;
+        while(i <= j){
+            if(Math.abs(nums[i]) >= Math.abs(nums[j])){
+                res[p--] = nums[i]*nums[i];
+                i++;
+            }
+            else{
+                res[p--] = nums[j]*nums[j];
+                j--;
+            }
+        }
+        return res;
+    }
+}
