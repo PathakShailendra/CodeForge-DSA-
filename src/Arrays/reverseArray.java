0c8bdbda8f678f73be25c98134599282a34137db
Shailendra Pathak <pathakharsh813@gmail.com>
2025-02-15 19:17:29 +0530

    reverse array gfg

---
+++ src/Arrays/reverseArray.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class reverseArray {
+    public void reverseArray(int arr[]) {
+        // code here
+        reverse(arr, 0, arr.length - 1);
+    }
+    private static void reverse(int[] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+}
