bf320dde155d7e8722faeb0c28675d2b97e18bc5
Shailendra Pathak <pathakharsh813@gmail.com>
2025-02-16 22:05:09 +0530

    largest element gfg

---
+++ src/Arrays/largstElementOfArray.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class largstElementOfArray {
+    public static int largest(int[] arr) {
+        int max = arr[0];
+        for(int num : arr) {
+            if(num > max){
+                max = num;
+            }
+        }
+        return max;
+    }
+}
