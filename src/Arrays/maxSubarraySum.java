51c02c31789b216cf69f03f37c6785791461d831
Shailendra Pathak <pathakharsh813@gmail.com>
2025-02-22 13:40:21 +0530

    53 leetcode

---
+++ src/Arrays/maxSubarraySum.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+public class maxSubarraySum {
+    public int maxSubArray(int[] nums) {
+        int currentSum = 0;
+        int maxSum = nums[0];
+        for(int i = 0; i < nums.length; i++){
+            currentSum += nums[i];
+            maxSum = Math.max(currentSum, maxSum);
+            if(currentSum < 0)currentSum = 0;
+        }
+        return maxSum ;
+    }
+}
