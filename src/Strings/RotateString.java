8b312a96b341c7a5023939b664fd262d1e5ec4de
Shailendra Pathak <pathakharsh813@gmail.com>
2025-02-27 18:18:46 +0530

    796 leetcode

---
+++ src/Strings/RotateString.java
@@ -0,0 +1,7 @@
+package Strings;
+
+public class RotateString {
+    public boolean rotateString(String s, String goal) {
+        return s.length()==goal.length() && (s+s).contains(goal);
+    }
+}
