ddb17d27a9ef68dddb9f8f0880f16e6c0eace9ee
Shailendra Pathak <pathakharsh813@gmail.com>
2025-05-07 18:39:24 +0530

    114 leetcode

---
+++ src/BinaryTrees/FlattenBinaryTreeUsingRecursion.java
@@ -0,0 +1,15 @@
+package BinaryTrees;
+
+public class FlattenBinaryTreeUsingRecursion {
+    TreeNode prev = null;
+    public void flatten(TreeNode root) {
+        if (root == null) return;
+
+        flatten(root.right);
+        flatten(root.left);
+
+        root.right = prev;
+        root.left = null;
+        prev = root;
+    }
+}
