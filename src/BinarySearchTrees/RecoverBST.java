c63af0d4e1fbfba373f15ef2151d3e0d6998a87d
Shailendra Pathak <pathakharsh813@gmail.com>
2025-05-09 18:50:52 +0530

    99 leetcode

---
+++ src/BinarySearchTrees/RecoverBST.java
@@ -0,0 +1,28 @@
+package BinarySearchTrees;
+
+public class RecoverBST {
+    TreeNode first = null, second = null, prev = new TreeNode(Integer.MIN_VALUE);
+
+    public void recoverTree(TreeNode root) {
+        inorder(root);
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+    }
+
+    private void inorder(TreeNode root) {
+        if (root == null) return;
+
+        inorder(root.left);
+
+        // Detect swapped nodes
+        if (prev.val > root.val) {
+            if (first == null) first = prev;
+            second = root;
+        }
+
+        prev = root;
+
+        inorder(root.right);
+    }
+}
