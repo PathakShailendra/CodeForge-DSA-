6f1bf55fadba3374dc1bab4017fff5977e9cc287
Shailendra Pathak <pathakharsh813@gmail.com>
2025-05-28 17:52:19 +0530

    SetBit

--- src/BitManipulation/GetBit.java
+++ src/BitManipulation/GetBit.java
@@ -1,6 +1,6 @@
 package BitManipulation;
 
-public class KthBitSetOrNot {
+public class GetBit {
     static boolean checkKthBit(int n, int k) {
         // Your code here
         return ((n & (1 << k)) != 0);
