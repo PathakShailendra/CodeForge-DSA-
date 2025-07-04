ec09c7e3fd527977bd59fac1f087bc5614595a69
Shailendra Pathak <pathakharsh813@gmail.com>
2025-07-04 13:24:53 +0530

    leetcode 120 tab

---
+++ src/DynamicProgramming/TriangleTab.java
@@ -0,0 +1,25 @@
+package DynamicProgramming;
+
+import java.util.List;
+
+public class TriangleTab {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+
+        int[][] dp = new int[n][n];
+
+        for (int j = 0; j < n; j++) {
+            dp[n - 1][j] = triangle.get(n - 1).get(j);
+        }
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                int down = dp[i + 1][j];
+                int diag = dp[i + 1][j + 1];
+                dp[i][j] = triangle.get(i).get(j) + Math.min(down, diag);
+            }
+        }
+
+        return dp[0][0];
+    }
+}
