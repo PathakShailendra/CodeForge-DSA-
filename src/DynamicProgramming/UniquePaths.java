d51f1617a172c9d6e845425122c7f2faf2736aeb
Shailendra Pathak <pathakharsh813@gmail.com>
2025-07-04 11:45:46 +0530

    leetcode 62

---
+++ src/DynamicProgramming/UniquePaths.java
@@ -0,0 +1,27 @@
+package DynamicProgramming;
+
+import java.util.Arrays;
+
+public class UniquePaths {
+    public int solve(int i, int j, int m, int n, int[][] dp){
+        if(i == m-1 && j == n-1){
+            return 1;
+        }
+        if(i >= m || j >= n){
+            return 0;
+        }
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int way1 = solve(i, j+1, m, n, dp);
+        int way2 = solve(i+1, j, m ,n, dp);
+        return dp[i][j] = way1+way2;
+    }
+
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        for(int i = 0; i < m; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        return solve(0,0,m,n, dp);
+    }
+}
