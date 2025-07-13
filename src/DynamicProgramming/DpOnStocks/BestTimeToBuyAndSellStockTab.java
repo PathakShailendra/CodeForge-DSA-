473a0b898c0d31b4f72f9f690d2f3244c43fbdb5
Shailendra Pathak <pathakharsh813@gmail.com>
2025-07-13 11:50:05 +0530

    leetcode 121 tab

---
+++ src/DynamicProgramming/DpOnStocks/BestTimeToBuyAndSellStockTab.java
@@ -0,0 +1,19 @@
+package DynamicProgramming.DpOnStocks;
+
+public class BestTimeToBuyAndSellStockTab {
+    public int maxProfit(int[] prices) {
+        int n = prices.length;
+        int[][] dp = new int[n + 1][2];
+
+        dp[n][0] = dp[n][1] = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][0] = Math.max(-prices[i] + dp[i + 1][1], dp[i + 1][0]);
+
+
+            dp[i][1] = Math.max(prices[i], dp[i + 1][1]);
+        }
+
+        return dp[0][0];
+    }
+}
